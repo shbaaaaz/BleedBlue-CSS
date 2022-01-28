d40b1ac6e714c364010af38499960c60746d40b2
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2022-01-29 01:04:22 +0530

    added alert and avatar components and some hover effect on button and links

---
+++ bleedbluedocs/docs.js
@@ -0,0 +1,6 @@
+const alertCloseExample = document.querySelector("#alert-close-example");
+const btnAlertCloseExample = document.querySelector("#btn-alert-close-example");
+
+btnAlertCloseExample.addEventListener("click", ()=>{
+    alertCloseExample.style.display = "none";
+});
