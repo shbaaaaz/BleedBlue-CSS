2480354706dafd2a12c79553967413668ae93245
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2022-02-01 18:41:21 +0530

    Added input component in the documentation website

--- bleedbluedocs/docs.js
+++
@@ -1,6 +0,0 @@
-const alertCloseExample = document.querySelector("#alert-close-example");
-const btnAlertCloseExample = document.querySelector("#btn-alert-close-example");
-
-btnAlertCloseExample.addEventListener("click", ()=>{
-    alertCloseExample.style.display = "none";
-});
